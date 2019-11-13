8d4bc5aaf2f9dd24aabb6b82acc5ffb575c779b0
Mauricio Grejanin <mgrejanin@eurekaria.com>
2019-11-12 23:28:23 -0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const ROOT = path.resolve(__dirname, "src");
+const DESTINATION = path.resolve(__dirname, "dist");
+
+module.exports = {
+  context: ROOT,
+
+  entry: {
+    main: "./main.ts"
+  },
+
+  output: {
+    filename: "[name].bundle.js",
+    path: DESTINATION
+  },
+
+  resolve: {
+    extensions: [".ts", ".js"],
+    modules: [ROOT, "node_modules"]
+  },
+
+  module: {
+    rules: [
+      /****************
+       * PRE-LOADERS
+       *****************/
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        use: "source-map-loader"
+      },
+      {
+        enforce: "pre",
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: "tslint-loader"
+      },
+
+      /****************
+       * LOADERS
+       *****************/
+      {
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        use: "awesome-typescript-loader"
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+
+  devtool: "cheap-module-source-map",
+  devServer: {}
+};
