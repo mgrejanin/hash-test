63c23aa254f95f6e2f0d1129dbd0d852df9b10ff
Mauricio Grejanin <maurici.gresanin.ext@panonline.com.br>
2019-11-17 17:54:01 -0300

    lógica dos cálculos

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,14 @@
-import { fromEvent } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import './style.scss';
-import { HashMask } from './mask';
+import { fromEvent } from "rxjs";
+import { HashMask } from "./mask";
+import "./style.scss";
 
 export class HashTest {
-  fractionDigits = 2;
-  valor: number;
   form: HTMLFormElement;
   inputs: {
     valor: HTMLInputElement;
     qtdParcelas: HTMLInputElement;
     mdr: HTMLInputElement;
   };
-
   results: {
     amanha: HTMLSpanElement;
     quinze: HTMLSpanElement;
@@ -21,34 +17,94 @@ export class HashTest {
   };
 
   constructor(private hashMask: HashMask) {
-    this.form = <HTMLFormElement>document.getElementById('form');
+    this.form = <HTMLFormElement>document.getElementById("form");
     this.inputs = {
-      valor: <HTMLInputElement>document.getElementById('input_valor'),
-      qtdParcelas: <HTMLInputElement>document.getElementById('input_parcelas'),
-      mdr: <HTMLInputElement>document.getElementById('input_mdr')
+      valor: <HTMLInputElement>document.getElementById("input_valor"),
+      qtdParcelas: <HTMLInputElement>document.getElementById("input_parcelas"),
+      mdr: <HTMLInputElement>document.getElementById("input_mdr")
     };
     this.results = {
-      amanha: <HTMLSpanElement>document.getElementById('result_amanha'),
-      quinze: <HTMLSpanElement>document.getElementById('result_15-dias'),
-      trinta: <HTMLSpanElement>document.getElementById('result_30-dias'),
-      noventa: <HTMLSpanElement>document.getElementById('result_90-dias')
+      amanha: <HTMLSpanElement>document.getElementById("result_amanha"),
+      quinze: <HTMLSpanElement>document.getElementById("result_15-dias"),
+      trinta: <HTMLSpanElement>document.getElementById("result_30-dias"),
+      noventa: <HTMLSpanElement>document.getElementById("result_90-dias")
     };
-    fromEvent(this.form, 'input')
-      .pipe(tap(() => this.calculate()))
-      .subscribe();
+    this.setListeners();
+  }
+
+  setListeners() {
+    fromEvent(this.form, "input").subscribe(() => {
+      this.calculate();
+    });
+
+    fromEvent(this.inputs.valor, "input").subscribe(() => {
+      this.hashMask.setMask(this.inputs.valor);
+    });
   }
 
   calculate() {
-    this.hashMask.setMask(this.inputs.valor);
     if (!this.validForm()) {
       return;
     }
+    let result = this.calcular();
+    this.results.amanha.innerText = result[0];
+    this.results.quinze.innerText = result[1];
+    this.results.trinta.innerText = result[2];
+    this.results.noventa.innerText = result[3];
+  }
 
-    this.results.amanha.innerText = (155030.1).toLocaleString('pt-BR', {
-      style: 'currency',
-      currency: 'BRL',
+  currencyFormat(valor: number) {
+    return valor.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
       minimumFractionDigits: 2
     });
+  }
+
+  calcular(): Array<string> {
+    let periodos = [1, 15, 30, 60];
+    let intervaloDeParcelas: Array<number> = [];
+    let mdrParsed = parseFloat(this.inputs.mdr.value);
+
+    for (
+      let index = parseInt(this.inputs.qtdParcelas.value);
+      index > 0;
+      index--
+    ) {
+      intervaloDeParcelas.push(30 * index);
+    }
+
+    // Dividindo valor total pela quantidade de parcelas
+    let vlrRecebivel =
+      parseInt(this.hashMask.textToNumber(this.inputs.valor.value)) /
+      parseInt(this.inputs.qtdParcelas.value);
+
+    // Deduzindo MDR do valor das parcelas
+    vlrRecebivel = vlrRecebivel * ((100 - mdrParsed) / 100);
+
+    return periodos.map(periodo => {
+      return this.currencyFormat(
+        intervaloDeParcelas
+          .reduce((acc, atual) => {
+            acc.push(
+              (this.formatReal(vlrRecebivel) *
+                (100 - (mdrParsed / 30) * (atual - periodo))) /
+                100
+            );
+            return acc;
+          }, [])
+          .reduce((acc, atual) => (acc += atual), 0)
+      );
+    });
+  }
+
+  formatReal(int: number) {
+    let tmp = int.toString().replace(/([0-9]{2})$/g, ".$1");
+    if (tmp.length > 6) {
+      tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+    }
+
+    return parseFloat(tmp);
   }
 
   validForm(): boolean {
